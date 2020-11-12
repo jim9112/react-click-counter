f63d315a0f04b5fee3f602e5154875a74bbd4828
jim9112 <jim9112@gmail.com>
2020-11-12 15:06:38 -0500

    add clicking and counting functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [clicks, setClicks] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Number of clicks: {clicks} </p>
+      <button type="button" onClick={() => setClicks(clicks + 1)}>
+        Click Me
+      </button>
     </div>
   );
 }
